8f3f9db06b3284c61108fb70df0787788e397c83
Raisky34 <raisky34@gmail.com>
2014-08-17 16:32:15 +0300

    Create BundleConfig.cs

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+using System.Web;
+using System.Web.Optimization;
+
+namespace MySite
+{
+    public class BundleConfig
+    {
+        // Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
+            // используйте средство построения на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/Foundation/css").Include(
+            "~/Content/css/foundation.min.css",
+            "~/Content/css/normalize.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js/foundation").Include(
+                        "~/Scripts/js/foundation/foundation.js",
+                        "~/Scripts/js/foundation/foundation.*"));
+        }
+    }
+}
