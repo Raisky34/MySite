6f2558710eef9c8543cf730786b04c39fb995e1d
Raisky34 <raisky34@gmail.com>
2014-08-17 16:39:23 +0300

    Create _references.js

---
+++ Scripts/_references.js
@@ -0,0 +1,6 @@
+/// <autosync enabled="true" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="respond.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="jquery-2.1.1.js" />
